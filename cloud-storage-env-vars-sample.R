0eb2e85381be2fec73b4bccc341e71be05d2ab28
Adam Carrier <adambcarrier@gmail.com>
2019-09-06 18:23:00 -0400

    Clarified which Google Cloud credentials are needed

--- cloud-storage-env-vars-sample.R
+++ cloud-storage-env-vars-sample.R
@@ -3,8 +3,10 @@ GCS_CLIENT_ID = "mykey"
 GCS_CLIENT_SECRET = "mysecretkey"
 GCS_DEFAULT_BUCKET = "my-default-bucket"
 
-# Needs to be a Service Account Key (see https://console.cloud.google.com/apis/credentials)
+# Needs to be a Service account key (not an API key or OAuth 2.0 client ID)
+# Create credentials here: https://console.cloud.google.com/apis/credentials
 GCS_AUTH_JSON_FILE = "service-auth.json"
 
 # See here for API keys: https://console.cloud.google.com/apis/credentials
+# Your Google Maps API key
 GC_API_KEY = "myapikey"
